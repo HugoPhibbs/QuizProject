bf33888eda8316ff6bbaf63126ba93d0a2230525
hugophibbs <81545721+hugophibbs@users.noreply.github.com>
2021-06-26 14:45:24 +1200

    Update Deck.java
    Changed dateOfCreation to type LocalDate
    
    implemented equals() and toString() methods.
    
    Adjusted addCard(FlashCard) method to check if cards already contains a FlashCard.
    
    Moved currentDate into parameters for cardsToQuiz()
    
    plus other minor changes

--- src/coreObjects/Deck.java
+++ src/coreObjects/Deck.java
@@ -16,12 +16,12 @@ public class Deck {
 	
 	/** String for the name of this deck **/
 	private String name;
-	/** String for the description of a deck **/
+	/** String for the description of a deck. E.g. a short note on what this deck contains**/
 	private String description;
 	/** ArrayList containing the cards for this deck **/
 	private ArrayList<FlashCard> cards = new ArrayList<FlashCard>();
-	/** int for the date of creation of this deck **/
-	private int dateOfCreation;
+	/** LocalDate object for the date of creation of this deck **/
+	private LocalDate dateOfCreation;
 	
 	
 	/** Constructor for a Deck object
@@ -29,30 +29,55 @@ public class Deck {
 	 * @param name String for the name of a deck
 	 * @param dateOfCreation int for the date of creation of a deck
 	 */
-	public Deck(String name, String description, int dateOfCreation){
+	public Deck(String name, String description, LocalDate dateOfCreation){
 		this.name = name;
 		this.description = description;
 		this.dateOfCreation = dateOfCreation;
 	}
 	
+	/** Returns a String representation of a Deck object
+	 * 
+	 */
+	@Override
+	public String toString() {
+		return String.format("Deck has name: %s, containing %d cards", name, this.size());
+	}
+	
+	/** Determines if two deck objects are equal to each other. 
+	 * Checks if their name and size are equal.
+	 * 
+	 */
+	@Override
+	public boolean equals(Object object) {
+		if (object instanceof Deck) {
+			Deck deck = (Deck) object;
+			return (deck.getName().equals(this.name) && 
+					deck.size() == this.size());
+		}
+		return false;
+	}
+	
 	/** Method for adding a card to a deck
-	 * All it does is add to the cards ArrayList. Made it into a method because
-	 * this will often be called and is cleaner than a Deck.getCards.addCard(card)
 	 * 
 	 * @param flashCard FlashCard object to be added to a deck
+	 * @returns boolean if a flashCard was added or not
 	 */
-	public void addCard(FlashCard flashCard) {
-		cards.add(flashCard);
+	public boolean addCard(FlashCard flashCard) {
+		if (!cards.contains(flashCard)) {
+			return (cards.add(flashCard));
+		}
+		else {
+			return false;
+		}
 	}
 	
 	/** Method for removing a card from a deck
-	 * All it does is remove a card from the cards ArrayList. Made it into a method because
-	 * this will often be called and is cleaner than a Deck.getCards.removeCard(card)
 	 * 
 	 * @param flashCard FlashCard object to be removed from a deck
+	 * @return boolean if the flashCard was removed from cards, ie it was found or not. 
 	 */
-	public void removeCard(FlashCard flashCard) {
-		cards.remove(flashCard);
+	public boolean removeCard(FlashCard flashCard) {
+		return (cards.remove(flashCard));
 	}
 	
 	/** Returns the cards that a user is to be quizzed on. Shuffles the deck before choosing new cards. 
@@ -60,16 +85,17 @@ public class Deck {
 	 * quizzed. 
 	 * 
 	 * @param maxNewCards int for the max number of new cards that a user wants to see
+	 * @param currentDate LocalDate object for the current date
 	 * @return ArrayList<FlashCard> for the cards to be quizzed on
 	 */
-	public ArrayList<FlashCard> cardsToQuiz(int maxNewCards){
+	public ArrayList<FlashCard> cardsToQuiz(int maxNewCards, LocalDate currentDate){
 		ArrayList<FlashCard> cardsToQuiz = new ArrayList<FlashCard>();
 		
 		Collections.shuffle(cards);
-		LocalDate currentDate = LocalDate.now();
 		int newCardsAdded = 0;
+		
 		for (FlashCard flashCard: cards) {
-			if (flashCard.getNextReviewDate() == null && newCardsAdded <= maxNewCards) {
+			if (flashCard.isNew() && newCardsAdded < maxNewCards) {
 				// Card is new, and added new cards is less than max
 				cardsToQuiz.add(flashCard);
 				newCardsAdded += 1;
@@ -84,6 +110,8 @@ public class Deck {
 	/** Method that returns the size of a deck. 
 	 * Again borrows functionality from ArrayList, as this will often be called
 	 * and is cleaner than Deck.getCards.size()
+	 * Keep as a method and not an attribute, as having it as an attribute means
+	 * that it need to be constantly updated
 	 * 
 	 * @return int for the size of deck
 	 */
@@ -101,9 +129,9 @@ public class Deck {
 	
 	/** Getter method for the date of creation of deck
 	 * 
-	 * @return int for the date of creation of a deck
+	 * @return LocalDate for the date of creation of a deck
 	 */
-	public int getDateOfCreation() {
+	public LocalDate getDateOfCreation() {
 		return dateOfCreation;
 	}
 	
@@ -137,5 +165,14 @@ public class Deck {
 	 */
 	public void setDescription(String description) {
 		this.description = description;
+	}
+	
+	/** Setter method for the cards of a deck. 
+	 * ONLY TO BE USED FOR TESTING
+	 * 
+	 * @param cards ArrayList<FlashCard> to be set as the cards for this deck
+	 */
+	public void setCards(ArrayList<FlashCard> cards) {
+		this.cards = cards;
 	}
 }
