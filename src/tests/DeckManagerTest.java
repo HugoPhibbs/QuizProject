5929424a6ca75c04e742b09452026660ff7088ee
hugophibbs <81545721+hugophibbs@users.noreply.github.com>
2021-06-27 15:48:49 +1200

    Create DeckManagerTest.java
    Need to implement these tests

---
+++ src/tests/DeckManagerTest.java
@@ -0,0 +1,19 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*; 
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class DeckManagerTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+	
+	@Test
+	static void findDeckTest() {
+		// TODO 
+	}
+
+}
