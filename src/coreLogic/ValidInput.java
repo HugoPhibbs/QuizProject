2274ed3af99ca344f3f89ab6a81db0190eea58e0
hugophibbs <81545721+hugophibbs@users.noreply.github.com>
2021-06-27 15:47:46 +1200

    Create ValidInput.java
    Need to implement useful methods

---
+++ src/coreLogic/ValidInput.java
@@ -0,0 +1,14 @@
+package coreLogic;
+
+/** Class to containing methods to check if input 
+ * from a user is valid 
+ * 
+ * @author Hugo Phibbs
+ * @author Tom Berry
+ * @version 27/6/21
+ * @since 27/6/21
+ *
+ */
+public class ValidInput {
+	
+}
