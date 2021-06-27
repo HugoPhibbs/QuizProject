63f52a3b3b736ea7b7ad2ab1dfa6d1045efec8ce
hugophibbs <81545721+hugophibbs@users.noreply.github.com>
2021-06-27 15:46:16 +1200

    Create AppEnvironment.java
    Created class.
    
    Need to implement

---
+++ src/coreLogic/AppEnvironment.java
@@ -0,0 +1,14 @@
+package coreLogic;
+
+/** Class to manage the environment of the application. 
+ * Holds all the objects necessary for a the application
+ * 
+ * @author Hugo Phibbs
+ * @author Tom Berry
+ * @version 27/6/21
+ * @since 27/6/21
+ * 
+ */
+public class AppEnvironment {
+
+}
