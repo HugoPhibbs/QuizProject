32246ee2026c05f621952143784bf8cbb29b5763
Tom Berry <tom.wberry5@gmail.com>
2021-06-28 17:32:16 +1200

    Statistics classes
    Added statistics classes and updated class path

---
+++ src/coreLogic/QuizStats.java
@@ -0,0 +1,13 @@
+package coreLogic;
+
+/** Quiz statistics for an individual quiz completed by the user.
+ * 
+ * @author Tom Berry
+ * @author Hugo Phibbs
+ * @version 28/06/2021
+ * @since 28/07/2021
+ *
+ */
+public class QuizStats {
+
+}
---
+++ src/coreLogic/UserStats.java
@@ -0,0 +1,13 @@
+package coreLogic;
+
+/** Overall user statistics for a user's quiz history.
+ * 
+ * @author Tom Berry
+ * @author Hugo Phibbs
+ * @version 28/06/2021
+ * @since 28/07/2021
+ *
+ */
+public class UserStats {
+
+}
