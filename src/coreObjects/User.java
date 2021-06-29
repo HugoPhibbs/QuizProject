726a9ee12b671043396abee335140fc9ff459e63
hugophibbs <81545721+hugophibbs@users.noreply.github.com>
2021-06-29 21:27:35 +1200

    Update User.java
    Now implements serializable and has an attribute for userStats

--- src/coreObjects/User.java
+++ src/coreObjects/User.java
@@ -1,5 +1,9 @@
 package coreObjects;
 
+import java.io.Serializable;
+
+import coreLogic.UserStats;
+
 /** Represents a user of the quiz application.
  * 
  * @author Hugo Phibbs
@@ -8,10 +12,29 @@ package coreObjects;
  * @since 27/6/21
  *
  */
-public class User {
+public class User implements Serializable {
 	
 	/** Name of a User */
 	private String name;
+	/** UserStats for this User */
+	private UserStats userStats = new UserStats();
+	
+	/** Constructor for a User object
+	 * 
+	 * @param name String for the name of a User
+	 */
+	public User(String name) {
+		this.name = name;
+	}
+	
+	/** Constructor for a User object
+	 * 
+	 */
+	public User() {
+		/* TODO may want to remove other constructor if we aren't using a Users name in the application -
+		 * add then remove name attribute as it won't be used
+		 */
+	}
 	
 	/** Getter method for the name of a user
 	 * 
@@ -27,6 +50,22 @@ public class User {
 	 */
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	/** Getter method for userStats
+	 * 
+	 * @return the userStats
+	 */
+	public UserStats getUserStats() {
+		return userStats;
+	}
+
+	/** Setter method for userStats
+	 * 
+	 * @param userStats the userStats to set
+	 */
+	public void setUserStats(UserStats userStats) {
+		this.userStats = userStats;
 	}
 
 }
