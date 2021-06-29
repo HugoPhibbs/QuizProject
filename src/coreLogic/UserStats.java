d638c18b6a9994c4127665a6c18c38044590644a
hugophibbs <81545721+hugophibbs@users.noreply.github.com>
2021-06-29 21:28:52 +1200

    Update UserStats.java
    Added methods, need to implement

--- src/coreLogic/UserStats.java
+++ src/coreLogic/UserStats.java
@@ -1,13 +1,39 @@
 package coreLogic;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+
 /** Overall user statistics for a user's quiz history.
+ * <p>
+ * Holds statistics for all the quizzes that a User has ever done
+ * <p>
+ * Creates graphs if requested representing trends from it's ArrayList of QuizStats 
+ * entries
  * 
  * @author Tom Berry
  * @author Hugo Phibbs
- * @version 28/06/2021
+ * @version 29/06/2021
  * @since 28/07/2021
  *
  */
-public class UserStats {
+public class UserStats implements Serializable {
+	
+	/** ArrayList containing QuizStats for all the quizzes that a User has ever
+	 * completed */
+	ArrayList<QuizStats> quizStatsEntries = new ArrayList<QuizStats>();
+	
+	/** Adds a QuizStats entry to quizStatsEntries */
+	public void addQuizStatsEntry(QuizStats quizStats) {
+		quizStatsEntries.add(quizStats);
+		
+		// TODO implement further - may be further things needed idk
+	}
+	
+	public void createGraphs() {
+		// TODO implement
+		/* May want to split this up into different methods
+		 * eg createAverageCorrectGraph(), createAverageWrongGraph()
+		 */
+	}
 
 }
