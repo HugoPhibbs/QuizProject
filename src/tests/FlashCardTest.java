1fbf7f6464971f8943e29a356366c628c21714ff
hugophibbs <81545721+hugophibbs@users.noreply.github.com>
2021-06-29 13:02:09 +1200

    Update FlashCardTest.java
    Refactored test class attributes

--- src/tests/FlashCardTest.java
+++ src/tests/FlashCardTest.java
@@ -5,23 +5,28 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.time.LocalDate;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import coreObjects.FlashCard;
 
 class FlashCardTest {
+	
+	FlashCard testCard1;
+	FlashCard testCard2;
+	FlashCard testCard3;
+	FlashCard testCard4;
 
-	@BeforeAll
-	static void setUpBeforeClass() throws Exception {
+	@BeforeEach
+	public void setUpBeforeClass() throws Exception {
+		testCard1 = new FlashCard("A", "B");
+		testCard2 = new FlashCard("A", "B");
+		testCard3 = new FlashCard("A", "A");
+		testCard4 = new FlashCard("B", "A");
 	}
 	
 	@Test
-	public void isDueTest() {
-		FlashCard testCard1 = new FlashCard("", "");
-		FlashCard testCard2 = new FlashCard("", "");
-		FlashCard testCard3 = new FlashCard("", "");
-		FlashCard testCard4 = new FlashCard("", "");
-		
+	public void isDueTest() {		
 		LocalDate testDueDate1 = LocalDate.of(2021, 6, 25);
 		testCard1.setNextReviewDate(testDueDate1);
 		
@@ -46,10 +51,7 @@ class FlashCardTest {
 	@SuppressWarnings("unlikely-arg-type")
 	@Test
 	public void equalsTest() {
-		FlashCard testCard1 = new FlashCard("A", "B");
-		FlashCard testCard2 = new FlashCard("A", "B");
-		FlashCard testCard3 = new FlashCard("A", "A");
-		FlashCard testCard4 = new FlashCard("B", "A");
+
 		String testString = new String("");
 		
 		assertEquals(true, testCard1.equals(testCard2));
@@ -57,6 +59,23 @@ class FlashCardTest {
 		assertEquals(false, testCard2.equals(testCard4)); 
 		// Test with a different object type
 		assertEquals(false, testCard3.equals(testString));
+	}
+	
+	@Test
+	public void updateNextReviewDateTest() {
+		// Test with a card that hasn't been tested before
+		LocalDate currentDate = LocalDate.now();
+		LocalDate expectedNextReviewDate1 = currentDate.plusDays(1);
+		testCard1.updateNextReviewDate();
+		assertEquals(expectedNextReviewDate1, testCard1.getNextReviewDate());
+		assertEquals(1, testCard1.getTimesReviewed()); // Check timesReviwed is updated
+		
+		// Test with a card that has been seen before
+		testCard2.setNextReviewDate(currentDate);
+		testCard2.setTimesReviewed(2);
+		testCard2.updateNextReviewDate();
+		LocalDate expectedNextReviewDate2 = currentDate.plusDays(5);
+		assertEquals(expectedNextReviewDate2, testCard2.getNextReviewDate());
 	}
 }
  	
