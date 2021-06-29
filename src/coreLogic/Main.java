1dc5e444b4d359d052435568afe83ae4414790ac
hugophibbs <81545721+hugophibbs@users.noreply.github.com>
2021-06-29 13:10:06 +1200

    Create Main.java
    Need to implement further

---
+++ src/coreLogic/Main.java
@@ -0,0 +1,22 @@
+package coreLogic;
+
+/** Class to start the Quiz Application
+ * contains the main Method to start the application
+ * 
+ * @author Hugo Phibbs
+ * @author Tom Berry
+ * @version 29/6/21
+ * @since 25/6/21
+ *
+ */
+public class Main {
+	
+	/** Method to start the application
+	 * 
+	 * @param args Arguments to be parsed for the starting of the application
+	 */
+	public static void main(String[] args) {
+		// TODO implement
+	}
+
+}
