713f7ff6abe47ea6beaca4119ae0296b656e2cc4
hugophibbs <81545721+hugophibbs@users.noreply.github.com>
2021-06-30 15:15:06 +1200

    Create Main.java

---
+++ src/coreLogic/Main.java
@@ -0,0 +1,19 @@
+package coreLogic;
+
+/** Class to start the application
+ * <p>
+ * Creates a new SetupScreen and and Setup object to setup the application
+ * 
+ * @author Hugo Phibbs
+ * @version 30/6/21
+ * @since 30/6/21
+ *
+ */
+public class Main {
+	
+	
+	public static void Main(String[] args) {
+		// 
+	}
+
+}
