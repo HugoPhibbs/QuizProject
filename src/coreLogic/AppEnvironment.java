91ae0c73a05bbebbaa7893be836799d522789bbc
hugophibbs <81545721+hugophibbs@users.noreply.github.com>
2021-07-02 13:04:06 +1200

    Update AppEnvironment.java
    Created new methods createQuiz(String) and viewDecks(). Supports key actions in GUI.
    
    Try to limit the amount of chaining methods in GUI, i.e. avoid using multiple getters and setters on one line, this can be intergrated into one method in the model classes.

--- src/coreLogic/AppEnvironment.java
+++ src/coreLogic/AppEnvironment.java
@@ -1,7 +1,9 @@
 package coreLogic;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 
+import coreObjects.Deck;
 import coreObjects.User;
 
 /** Class to manage the environment of the application. 
@@ -21,6 +23,39 @@ public class AppEnvironment implements Serializable {
 	private DeckManager deckManager = new DeckManager();
 	/** User object for this AppEnvironment */
 	private User user = new User();
+	
+	/** Creates a new quiz and returns and instance of FashCardQuiz
+	 * for a deck with name deckName, and for a User user
+	 * <p>
+	 * Used by GUI
+	 * 
+	 * @param deckName String for the name of a deck that is to be quizzed on
+	 * @return FlashCardQuiz object for a new quiz
+	 * @throws IllegalArgumentException if deckName isn't the name of 
+	 * any deck in the deckManager of this AppEnviornment
+	 */
+	public FlashCardQuiz newQuiz(String deckName) {
+		if (deckManager.containsDeck(deckName)) {
+			Deck quizDeck = deckManager.findDeck(deckName);
+			FlashCardQuiz newQuiz = new FlashCardQuiz(quizDeck, user.getUserStats());
+		    return newQuiz;
+		}
+		else {
+			throw new IllegalArgumentException("No deck with name deckName in this collection!");
+		}
+	}
+	
+	/** Allows a user to see what decks they have in the GUI
+	 * returns an ArrayList of Deck objects for the deck collection of
+	 * this AppEnvironment's deckManager
+	 * <p>
+	 * This can then be parsed by GUI into a JTable, from there a user can select a Deck to edit
+	 * 
+	 * @return ArrayList of Deck objects, as explained above. 
+	 */
+	public ArrayList<Deck> viewDecks() {
+		return (deckManager.getDeckCollection());
+	}
 	
 	/** Constructor for AppEnvironment
 	 * <p>
