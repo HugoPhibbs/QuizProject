727efec741680b7fb5df5a34a91268b0c17af91a
hugophibbs <81545721+hugophibbs@users.noreply.github.com>
2021-07-06 21:11:25 +1200

    Update EditDeckScreen.java
    Created key components,
    
    need to organise

--- src/gui/EditDeckScreen.java
+++ src/gui/EditDeckScreen.java
@@ -3,8 +3,22 @@ package gui;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import java.awt.Color;
+import javax.swing.JTable;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JTextField;
+import javax.swing.JTextPane;
 
-public class EditDeckScreen extends Screen{
+public class EditDeckScreen {
+	
+	JFrame frame;
+	private JTable table;
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -35,8 +49,91 @@ public class EditDeckScreen extends Screen{
 	 */
 	protected void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
+		frame.getContentPane().setLayout(null);
+		frame.setBounds(100, 100, 900, 900);
+		frame.getContentPane().setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(63, 127, 742, 467);
+		frame.getContentPane().add(panel);
+		panel.setLayout(null);
+		
+		table = new JTable();
+		table.setBounds(12, 13, 718, 441);
+		panel.add(table);
+		
+		JLabel lblNewLabel = new JLabel("Editing <deckName> ");
+		lblNewLabel.setBounds(335, 86, 138, 28);
+		frame.getContentPane().add(lblNewLabel);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBounds(367, 622, 182, 129);
+		frame.getContentPane().add(panel_1);
+		panel_1.setLayout(null);
+		
+		JButton btnNewButton_1 = new JButton("Add FlashCard");
+		btnNewButton_1.setBounds(12, 13, 160, 28);
+		panel_1.add(btnNewButton_1);
+		
+		JButton btnNewButton = new JButton("Edit FlashCard");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton.setBounds(12, 54, 160, 25);
+		panel_1.add(btnNewButton);
+		
+		JButton btnNewButton_3 = new JButton("Delete FlashCard");
+		btnNewButton_3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton_3.setBounds(12, 92, 160, 25);
+		panel_1.add(btnNewButton_3);
+		
+		JPanel panel_2 = new JPanel();
+		panel_2.setBounds(63, 622, 296, 129);
+		frame.getContentPane().add(panel_2);
+		panel_2.setLayout(null);
+		
+		JLabel lblNewLabel_1 = new JLabel("Change Deck name");
+		lblNewLabel_1.setBounds(97, 13, 124, 16);
+		panel_2.add(lblNewLabel_1);
+		
+		textField = new JTextField();
+		textField.setBounds(12, 37, 272, 22);
+		panel_2.add(textField);
+		textField.setColumns(10);
+		
+		JButton btnNewButton_4 = new JButton("Delete Deck");
+		btnNewButton_4.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton_4.setBounds(12, 91, 272, 25);
+		panel_2.add(btnNewButton_4);
+		
+		JPanel panel_3 = new JPanel();
+		panel_3.setBounds(561, 622, 244, 129);
+		frame.getContentPane().add(panel_3);
+		panel_3.setLayout(null);
+		
+		JButton btnNewButton_2 = new JButton("Continue");
+		btnNewButton_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton_2.setBounds(135, 90, 97, 25);
+		panel_3.add(btnNewButton_2);
+		
+		JTextPane textPane = new JTextPane();
+		textPane.setBounds(12, 13, 220, 64);
+		panel_3.add(textPane);
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
 
+
+	}
 }
