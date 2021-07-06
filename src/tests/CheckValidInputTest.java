b1a6248c5168a9c94631a3ce7c79234a889a1d6e
hugophibbs <81545721+hugophibbs@users.noreply.github.com>
2021-07-07 09:46:24 +1200

    Create CheckValidInputTest.java
    Added testing class
    
    Rehash from Island Trader Assignment, rewrote tests to reflect context of application

---
+++ src/tests/CheckValidInputTest.java
@@ -0,0 +1,31 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;  
+
+import org.junit.jupiter.api.*;
+
+import coreLogic.CheckValidInput;
+
+/** JUnit testing class for CheckValidInputClass
+ * 
+ * @author Hugo Phibbs 
+ * @version 7/7/2021
+ * @since 15/4/2021
+ */
+
+class CheckValidInputTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+	
+	@Test
+	void nameIsValidTest() {
+		assertEquals(true, CheckValidInput.nameIsValid("German"));
+		assertEquals(true, CheckValidInput.nameIsValid("Math Questions"));
+		assertEquals(false, CheckValidInput.nameIsValid(""));					// too few characters
+		assertEquals(false, CheckValidInput.nameIsValid("mathquestionsssssssss"));		// too many characters
+		assertEquals(false, CheckValidInput.nameIsValid("the%"));					// special character
+		assertEquals(false, CheckValidInput.nameIsValid("23jamie"));				// numbers present
+	}
+}
