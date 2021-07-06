184166c1b264055b1d3f1d1e145f09ca052f7cc2
hugophibbs <81545721+hugophibbs@users.noreply.github.com>
2021-07-06 21:33:34 +1200

    Update SetupScreen.java
    Added javadoc
    
    Put most components into panels... makes things easier

--- src/gui/SetupScreen.java
+++ src/gui/SetupScreen.java
@@ -1,6 +1,6 @@
 package gui;
 
-import java.awt.EventQueue;
+import java.awt.EventQueue; 
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -12,20 +12,38 @@ import setup.Setup;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.awt.event.ActionEvent;
 import java.awt.Color;
+import javax.swing.JPanel;
 
+/** Screen for setting up the application
+ * <p>
+ * Handles selecting the working/saving directory of this application and either creating
+ * or loading a previous saved session for a name of a user.
+ * 
+ * @author Hugo Phibbs
+ *
+ */
 public class SetupScreen {
 
 	private JFrame frame;
+	
+	/** JTextField for entering the working directory of this application */
 	private JTextField textFieldEnterDirectory;
+	/** Setup class for starting this application */
 	private Setup setup;
+	/** JButton to close from this screen and progress to the main screen, only progresses if it is permissable */
 	private JButton btnContinue;
+	/** JBUtton to load a session for the name of a user */
 	private JButton btnLoadSession;
+	/** JButton to create a session for the name of a user */
 	private JButton btnCreateSession;
+	/** JTextField to enter the name of a user */
 	private JTextField textFieldEnterName;
+	/** JLabel to display any errors to a user */
 	private JLabel lblNameError;
+	private JPanel panel_1;
+	private JPanel panel_2;
 
 	/**
 	 * Launch the application.
@@ -70,48 +88,20 @@ public class SetupScreen {
 		lblWelcome.setBounds(85, 7, 263, 46);
 		frame.getContentPane().add(lblWelcome);
 		
-		textFieldEnterDirectory = new JTextField();
-		textFieldEnterDirectory.setBounds(62, 74, 286, 22);
-		frame.getContentPane().add(textFieldEnterDirectory);
-		textFieldEnterDirectory.setColumns(10);
-		
-		JLabel lblEnterDirectory = new JLabel("Please enter a working directory!");
-		lblEnterDirectory.setFont(new Font("Tahoma", Font.PLAIN, 11));
-		lblEnterDirectory.setBounds(119, 52, 186, 15);
-		frame.getContentPane().add(lblEnterDirectory);
-		
-		textFieldEnterName = new JTextField();
-		textFieldEnterName.setBounds(165, 157, 66, 22);
-		frame.getContentPane().add(textFieldEnterName);
-		textFieldEnterName.setColumns(10);
-		
-		JLabel lblEnterName = new JLabel("Please enter your name!");
-		lblEnterName.setFont(new Font("Tahoma", Font.PLAIN, 11));
-		lblEnterName.setBounds(143, 127, 129, 23);
-		frame.getContentPane().add(lblEnterName);
-		
 		lblNameError= new JLabel();
 		lblNameError.setForeground(Color.RED);
 		lblNameError.setFont(new Font("Dialog", Font.PLAIN, 7));
 		lblNameError.setBounds(166, 222, 139, 16);
 		frame.getContentPane().add(lblNameError);
 		
-	}
-	
-	private void createButtons() {
-		
-		JButton btnNewButton = new JButton("Enter");
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				setup = new Setup(textFieldEnterDirectory.getText());
-				btnCreateSession.setEnabled(true );
-				btnLoadSession.setEnabled(true);
-			}
-		});
-		btnNewButton.setBounds(165, 103, 69, 17);
-		frame.getContentPane().add(btnNewButton);
+		JPanel panel = new JPanel();
+		panel.setBounds(49, 191, 352, 33);
+		frame.getContentPane().add(panel);
+		panel.setLayout(null);
 		
 		this.btnCreateSession = new JButton("Create Session");
+		btnCreateSession.setBounds(12, 12, 139, 17);
+		panel.add(btnCreateSession);
 		btnCreateSession.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				String username = textFieldEnterName.getText();
@@ -119,11 +109,11 @@ public class SetupScreen {
 				btnContinue.setEnabled(true);
 			}
 		});
-		btnCreateSession.setBounds(62, 193, 139, 17);
-		frame.getContentPane().add(btnCreateSession);
 		btnCreateSession.setEnabled(false);
 		
 		this.btnLoadSession = new JButton("Load Session");
+		btnLoadSession.setBounds(173, 12, 148, 17);
+		panel.add(btnLoadSession);
 		btnLoadSession.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				String userName = textFieldEnterName.getText();
@@ -137,9 +127,52 @@ public class SetupScreen {
 				
 			}
 		});
-		btnLoadSession.setBounds(219, 193, 148, 17);
-		frame.getContentPane().add(btnLoadSession);
 		btnLoadSession.setEnabled(false);
+		
+		panel_1 = new JPanel();
+		panel_1.setBounds(127, 132, 139, 58);
+		frame.getContentPane().add(panel_1);
+		panel_1.setLayout(null);
+		
+		JLabel lblEnterName = new JLabel("Please enter your name!");
+		lblEnterName.setBounds(12, 0, 129, 23);
+		panel_1.add(lblEnterName);
+		lblEnterName.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		
+		textFieldEnterName = new JTextField();
+		textFieldEnterName.setBounds(12, 24, 119, 22);
+		panel_1.add(textFieldEnterName);
+		textFieldEnterName.setColumns(10);
+		
+		panel_2 = new JPanel();
+		panel_2.setBounds(24, 46, 365, 75);
+		frame.getContentPane().add(panel_2);
+		panel_2.setLayout(null);
+		
+		JButton btnNewButton = new JButton("Enter");
+		btnNewButton.setBounds(139, 56, 69, 17);
+		panel_2.add(btnNewButton);
+		
+		textFieldEnterDirectory = new JTextField();
+		textFieldEnterDirectory.setBounds(43, 22, 286, 22);
+		panel_2.add(textFieldEnterDirectory);
+		textFieldEnterDirectory.setColumns(10);
+		
+		JLabel lblEnterDirectory = new JLabel("Please enter a working directory!");
+		lblEnterDirectory.setBounds(91, 0, 186, 15);
+		panel_2.add(lblEnterDirectory);
+		lblEnterDirectory.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				setup = new Setup(textFieldEnterDirectory.getText());
+				btnCreateSession.setEnabled(true );
+				btnLoadSession.setEnabled(true);
+			}
+		});
+		
+	}
+	
+	private void createButtons() {
 		
 		this.btnContinue = new JButton("Continue");
 		btnContinue.addActionListener(new ActionListener() {
