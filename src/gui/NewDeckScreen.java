17c9ae6eaa3b92f8e20a811868e18029918e7c0b
hugophibbs <81545721+hugophibbs@users.noreply.github.com>
2021-07-07 14:49:27 +1200

    Update NewDeckScreen.java
    Changed variable names
    
    put creating methods into components to make things neater
    
    added method stubs-- need to implement further

--- src/gui/NewDeckScreen.java
+++ src/gui/NewDeckScreen.java
@@ -3,10 +3,26 @@ package gui;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.JTextPane;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
+/** Represents a Screen to create a new Deck
+ * 
+ * @author Hugo Phibbs
+ *
+ */
 public class NewDeckScreen {
 
 	private JFrame frame;
+	/** TextField for entering the name of a new Deck */
+	private JTextField textFieldName;
+	/** TextField for entering the description of a new Deck */
+	private JTextField textFieldDescription;
 
 	/**
 	 * Launch the application.
@@ -38,6 +54,74 @@ public class NewDeckScreen {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		createDetailsPanel();
+		createContinuePanel();
+		createMiscComponents();
 	}
-
+	
+	/** Creates components contains in panelDeckDetails
+	 * 
+	 */
+	private void createDetailsPanel() {
+		JPanel panelDeckDetails = new JPanel();
+		panelDeckDetails.setBounds(12, 53, 213, 157);
+		frame.getContentPane().add(panelDeckDetails);
+		panelDeckDetails.setLayout(null);
+		
+		textFieldName = new JTextField();
+		textFieldName.setBounds(102, 21, 85, 22);
+		panelDeckDetails.add(textFieldName);
+		textFieldName.setColumns(10);
+		
+		textFieldDescription = new JTextField();
+		textFieldDescription.setBounds(5, 74, 196, 70);
+		panelDeckDetails.add(textFieldDescription);
+		textFieldDescription.setColumns(10);
+		
+		JLabel lblName = new JLabel("Deck name:");
+		lblName.setBounds(5, 24, 85, 16);
+		panelDeckDetails.add(lblName);
+		
+		JLabel lblDescription = new JLabel("Description:");
+		lblDescription.setBounds(5, 56, 92, 16);
+		panelDeckDetails.add(lblDescription);
+	}
+	
+	/** Creates components contains in panelContinue
+	 * 
+	 */
+	private void createContinuePanel() {
+		JPanel panelContinue = new JPanel();
+		panelContinue.setBounds(237, 53, 185, 157);
+		frame.getContentPane().add(panelContinue);
+		panelContinue.setLayout(null);
+		
+		JButton btnCreateDeck = new JButton("Create Deck");
+		btnCreateDeck.setBounds(30, 119, 128, 25);
+		panelContinue.add(btnCreateDeck);
+		btnCreateDeck.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				createDeck();
+			}
+		});
+		
+		JTextPane textPaneErrorMsg = new JTextPane();
+		textPaneErrorMsg.setBounds(17, 13, 156, 93);
+		panelContinue.add(textPaneErrorMsg);
+	}
+	
+	/** Creates various components that don't belong in a panel
+	 * 
+	 */
+	private void createMiscComponents() {
+		JLabel lblTitle = new JLabel("Creating a deck");
+		lblTitle.setBounds(154, 24, 96, 16);
+		frame.getContentPane().add(lblTitle);
+	}
+	
+	private void createDeck() {
+		// TODO implement
+	}
 }
