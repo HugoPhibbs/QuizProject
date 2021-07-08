c4e7667e5f825a1f4887852ea7149bca520844fd
hugophibbs <81545721+hugophibbs@users.noreply.github.com>
2021-07-08 13:15:51 +1200

    Update SetupScreen.java
    Refactored code for new changes in Setup.
    
    Refactoring code into methods for neatness and maintainability
    
    adding javadoc

--- src/gui/SetupScreen.java
+++ src/gui/SetupScreen.java
@@ -41,9 +41,13 @@ public class SetupScreen {
 	/** JTextField to enter the name of a user */
 	private JTextField textFieldEnterName;
 	/** JLabel to display any errors to a user */
-	private JLabel lblNameError;
-	private JPanel panel_1;
-	private JPanel panel_2;
+	private JLabel lblConfigSessionError;
+	/** JPanel to contain components relating to entering a user name for a session */
+	private JPanel panelEnterName;
+	/** JPanel to contain components relating to entering a directory for loading or creating a session */
+	private JPanel panelEnterDirectory;
+	/** JButton to enter a directory to create or load a session to */
+	private JButton btnEnterDirectory;
 
 	/**
 	 * Launch the application.
@@ -78,111 +82,189 @@ public class SetupScreen {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 	
-		createButtons();
-		createLabels();
+		createComponents();
+	}
+
+	// ******************** Creating Components  ******************* //
+
+	/** Calls methods to create components for this Screen */
+	private void createComponents() {
+		createMiscComponents();
+		createEnterNamePanel();
+		createConfigSessionPanel();
+		createEnterDirectoryPanel();
+	}
+
+	/** Creates and fills and Panel with components relating to entering a 
+	 * working directory for this application */
+	private void createEnterDirectoryPanel(){
+		panelEnterDirectory = new JPanel();
+		panelEnterDirectory.setBounds(24, 46, 365, 75);
+		frame.getContentPane().add(panelEnterDirectory);
+		panelEnterDirectory.setLayout(null);
+		
+		textFieldEnterDirectory = new JTextField();
+		textFieldEnterDirectory.setBounds(43, 22, 286, 22);
+		panelEnterDirectory.add(textFieldEnterDirectory);
+		textFieldEnterDirectory.setColumns(10);
+		
+		JLabel lblEnterDirectory = new JLabel("Please enter a working directory!");
+		lblEnterDirectory.setBounds(91, 0, 186, 15);
+		panelEnterDirectory.add(lblEnterDirectory);
+		lblEnterDirectory.setFont(new Font("Tahoma", Font.PLAIN, 11));
+
+		btnEnterDirectory = new JButton("Enter");
+		btnEnterDirectory.setBounds(139, 56, 69, 17);
+		panelEnterDirectory.add(btnEnterDirectory);
+
+		addBtnEnterDirectoryListener();
+	}
+
+	/** Creates and fill a Panel with components relating to creating or loading a session */
+	private void createConfigSessionPanel(){
+		JPanel panelConfigSession = new JPanel();
+		panelConfigSession.setBounds(49, 191, 352, 33);
+		frame.getContentPane().add(panelConfigSession);
+		panelConfigSession.setLayout(null);
+		
+		btnCreateSession = new JButton("Create Session");
+		btnCreateSession.setBounds(12, 12, 139, 17);
+		panelConfigSession.add(btnCreateSession);
+		btnCreateSession.setEnabled(false);
+		
+		btnLoadSession = new JButton("Load Session");
+		btnLoadSession.setBounds(173, 12, 148, 17);
+		panelConfigSession.add(btnLoadSession);
+		btnLoadSession.setEnabled(false);
+
+		addConfigSessionBtnListeners();
+	}
+
+	/** Creates and fills Panel for entering a user's name into to load or create a session */
+	private void createEnterNamePanel(){
+		panelEnterName = new JPanel();
+		panelEnterName.setBounds(127, 132, 139, 58);
+		frame.getContentPane().add(panelEnterName);
+		panelEnterName.setLayout(null);
+		
+		JLabel lblEnterName = new JLabel("Please enter your name!");
+		lblEnterName.setBounds(12, 0, 129, 23);
+		panelEnterName.add(lblEnterName);
+		lblEnterName.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		
+		textFieldEnterName = new JTextField();
+		textFieldEnterName.setBounds(12, 24, 119, 22);
+		panelEnterName.add(textFieldEnterName);
+		textFieldEnterName.setColumns(10);
 	}
 	
-	private void createLabels() {
+	/** Creates miscelanous components that aren't contained in a Panel */
+	private void createMiscComponents(){
 		JLabel lblWelcome = new JLabel("Hello, welcome to Quizzer!");
 		lblWelcome.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		lblWelcome.setBounds(85, 7, 263, 46);
 		frame.getContentPane().add(lblWelcome);
 		
-		lblNameError= new JLabel();
-		lblNameError.setForeground(Color.RED);
-		lblNameError.setFont(new Font("Dialog", Font.PLAIN, 7));
-		lblNameError.setBounds(166, 222, 139, 16);
-		frame.getContentPane().add(lblNameError);
-		
-		JPanel panel = new JPanel();
-		panel.setBounds(49, 191, 352, 33);
-		frame.getContentPane().add(panel);
-		panel.setLayout(null);
-		
-		this.btnCreateSession = new JButton("Create Session");
-		btnCreateSession.setBounds(12, 12, 139, 17);
-		panel.add(btnCreateSession);
-		btnCreateSession.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				String username = textFieldEnterName.getText();
-				setup.createSession(username);
-				btnContinue.setEnabled(true);
-			}
-		});
-		btnCreateSession.setEnabled(false);
-		
-		this.btnLoadSession = new JButton("Load Session");
-		btnLoadSession.setBounds(173, 12, 148, 17);
-		panel.add(btnLoadSession);
-		btnLoadSession.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				String userName = textFieldEnterName.getText();
-				try{
-					setup.loadSession(userName);
-					btnContinue.setEnabled(true);
-				}
-				catch (FileNotFoundException fnfe){
-					lblNameError.setText(String.format("Session for '%s' cannot be found!", userName));
-				}
-				
-			}
-		});
-		btnLoadSession.setEnabled(false);
-		
-		panel_1 = new JPanel();
-		panel_1.setBounds(127, 132, 139, 58);
-		frame.getContentPane().add(panel_1);
-		panel_1.setLayout(null);
-		
-		JLabel lblEnterName = new JLabel("Please enter your name!");
-		lblEnterName.setBounds(12, 0, 129, 23);
-		panel_1.add(lblEnterName);
-		lblEnterName.setFont(new Font("Tahoma", Font.PLAIN, 11));
-		
-		textFieldEnterName = new JTextField();
-		textFieldEnterName.setBounds(12, 24, 119, 22);
-		panel_1.add(textFieldEnterName);
-		textFieldEnterName.setColumns(10);
-		
-		panel_2 = new JPanel();
-		panel_2.setBounds(24, 46, 365, 75);
-		frame.getContentPane().add(panel_2);
-		panel_2.setLayout(null);
-		
-		JButton btnNewButton = new JButton("Enter");
-		btnNewButton.setBounds(139, 56, 69, 17);
-		panel_2.add(btnNewButton);
-		
-		textFieldEnterDirectory = new JTextField();
-		textFieldEnterDirectory.setBounds(43, 22, 286, 22);
-		panel_2.add(textFieldEnterDirectory);
-		textFieldEnterDirectory.setColumns(10);
-		
-		JLabel lblEnterDirectory = new JLabel("Please enter a working directory!");
-		lblEnterDirectory.setBounds(91, 0, 186, 15);
-		panel_2.add(lblEnterDirectory);
-		lblEnterDirectory.setFont(new Font("Tahoma", Font.PLAIN, 11));
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				setup = new Setup(textFieldEnterDirectory.getText());
-				btnCreateSession.setEnabled(true );
-				btnLoadSession.setEnabled(true);
-			}
-		});
-		
-	}
-	
-	private void createButtons() {
-		
-		this.btnContinue = new JButton("Continue");
-		btnContinue.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				setup.onSetupFinished();
-				frame.dispose(); // TODO implement this into Screen later, when design is finished!
-			}
-		});
+		lblConfigSessionError= new JLabel();
+		lblConfigSessionError.setForeground(Color.RED);
+		lblConfigSessionError.setFont(new Font("Dialog", Font.PLAIN, 7));
+		lblConfigSessionError.setBounds(166, 222, 139, 16);
+		frame.getContentPane().add(lblConfigSessionError);
+
+		btnContinue = new JButton("Continue");
 		btnContinue.setBounds(312, 241, 119, 17);
 		frame.getContentPane().add(btnContinue);
 		btnContinue.setEnabled(false);
+		addBtnContinueListener();
+	}
+
+	// ****************** Adding Listeners to Components ********************* //
+
+	/** Adds an action listener to btnEnterDirectory */
+	private void addBtnEnterDirectoryListener(){
+		btnEnterDirectory.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				directoryEntered();
+			}
+		});
+	}
+
+	/** Adds Action Listeners to buttons relating to loading and creatig a session */
+	private void addConfigSessionBtnListeners(){
+		btnCreateSession.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				createSession();;
+			}
+		});
+
+		btnLoadSession.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				loadSession();
+			}
+		});
+	}
+
+	/** Adds an Action Listener to btnContinue */
+	private void addBtnContinueListener() {
+		btnContinue.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				onContinue();
+			}
+		});
+	}
+
+	// *********************** Handling Listener Events ************************* //
+
+	/** Handles the entering of a Directory */
+	private void directoryEntered(){
+		setup = new Setup(textFieldEnterDirectory.getText());
+		btnCreateSession.setEnabled(true );
+		btnLoadSession.setEnabled(true);
+	}
+
+	/** Handles pressing of btnCreateSession */
+	private void createSession(){
+		try {
+			setup.createSession(userName());
+			btnContinue.setEnabled(true);
+		}
+		catch (IllegalArgumentException iae){
+			displayConfigSessionError(iae.getMessage());
+		}
+	}
+
+	/** Handles pressing of btnLoadSession 
+	 * Tries to load a session, displays an error in lblConfigSessionError if FileNotFoundException is caught */
+	private void loadSession(){
+		try{
+			setup.loadSession(userName());
+			btnContinue.setEnabled(true);
+		}
+		catch (FileNotFoundException fnfe){
+			displayConfigSessionError(fnfe.getMessage());
+		}
+	}
+
+	/** Handles pressing of btnContinue */
+	private void onContinue(){
+		setup.onSetupFinished();
+		frame.dispose(); // TODO implement this into Screen later, when design is finished!
+	}
+
+	/** Displays an error to user relating to configurating a session
+	 * , that is; loading or creating
+	 * 
+	 * @param msg String for error message to be displayed
+	 */
+	private void displayConfigSessionError(String msg){
+		lblConfigSessionError.setText(msg);
+	}
+
+	/** Returns the user name currently entered to load a session for 
+	 * 
+	 * @return String for the user name currently entered 
+	 */
+	private String userName(){
+		return textFieldEnterName.getText();
 	}
 }
