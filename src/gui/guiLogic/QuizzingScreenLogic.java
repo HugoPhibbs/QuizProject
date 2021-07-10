3cf16ee892dee9a09ce4e32b8c492f82d2c26a8a
hugophibbs <81545721+hugophibbs@users.noreply.github.com>
2021-07-10 16:47:11 +1200

    Create QuizzingScreenLogic.java

---
+++ src/gui/guiLogic/QuizzingScreenLogic.java
@@ -0,0 +1,32 @@
+package gui.guiLogic;
+
+import core.coreLogic.FlashCardQuiz;
+import core.coreObjects.FlashCard;
+import gui.guiShell.QuizzingScreen;
+
+public class QuizzingScreenLogic {
+
+    private FlashCardQuiz flashCardQuiz;
+    private QuizzingScreen screen;
+
+    public QuizzingScreenLogic(FlashCardQuiz flashCardQuiz, QuizzingScreen quizzingScreen){
+        this.flashCardQuiz = flashCardQuiz;
+        this.screen = quizzingScreen;
+    }
+
+    public void flashCardFlip() {
+		// TODO implement
+	}
+	
+	public void flashCardAgain() {
+		// TODO implement
+	}
+	
+	public void flashCardOk() {
+		// TODO implement
+	}
+	
+	public void finishQuiz() {
+		// TODO implement
+	}
+}
