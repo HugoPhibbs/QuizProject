4326eb106b7e9a7ad49e452b3d84e2aa35572260
hugophibbs <81545721+hugophibbs@users.noreply.github.com>
2021-07-11 14:35:37 +1200

    Update CreateDeckScreenLogic.java
    Now extends ScreenLogic

--- src/gui/guiLogic/CreateDeckScreenLogic.java
+++ src/gui/guiLogic/CreateDeckScreenLogic.java
@@ -1,5 +1,16 @@
 package gui.guiLogic;
 
-public class CreateDeckScreenLogic {
-    
+public class CreateDeckScreenLogic extends ScreenLogic {
+
+    public CreateDeckScreenLogic(ScreenLogic parentScreenLogic) {
+        super(parentScreenLogic);
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    protected void createScreen() {
+        // TODO Auto-generated method stub
+
+    }
+
 }
