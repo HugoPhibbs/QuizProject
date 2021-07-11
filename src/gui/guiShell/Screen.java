146b623043f8be20d7e540ad49ca16357fcee941
hugophibbs <81545721+hugophibbs@users.noreply.github.com>
2021-07-11 14:39:51 +1200

    Update Screen.java
    Moved it back to guiShell package
    
    added switchToParent() method, and toggleButton method

--- src/gui/guiShell/Screen.java
+++ src/gui/guiShell/Screen.java
@@ -1,15 +1,17 @@
-package gui.guiLogic;
+package gui.guiShell;
 
 import java.awt.Color;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
 import setup.Setup;
 
-/** Represents a Screen object for GUI
+/**
+ * Represents a Screen object for GUI
  * 
  * @author Hugo Phibbs and Jordan Vegar
  * @version 25/6/21
@@ -17,27 +19,29 @@ import setup.Setup;
  * 
  */
 public abstract class Screen {
-	
+
 	/** Frame object for this screen **/
 	JFrame frame;
 
 	/** Screen object that is the parent Screen for this Screen */
 	Screen parent;
-	
-	/** Empty Constructor for Screen, used until extensions of Screen need full constructor
+
+	/**
+	 * Empty Constructor for Screen, used until extensions of Screen need full
+	 * constructor
 	 * 
 	 */
-	Screen(){
+	Screen() {
 		// TODO Delete later!
 	}
-	
+
 	protected Screen(String title) {
 		this.frame = new JFrame();
 		frame.setTitle(title);
 		frame.getContentPane().setBackground(new Color(255, 222, 173));
 		setFrameCharacteristics();
 	}
-	
+
 	/** Sets the characteristics of the frame that are common to all screens. */
 	private void setFrameCharacteristics() {
 		// Prevent the user from quiting immediately when quit is clicked.
@@ -48,71 +52,88 @@ public abstract class Screen {
 				confirmQuit();
 			}
 		});
-		
+
 		frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 	}
-	
-	/** This is where the bounds of your frame will be set and methods which create the components
-	 * will be called. Is called at the end of the constructor method.
-	 *  */
+
+	/**
+	 * This is where the bounds of your frame will be set and methods which create
+	 * the components will be called. Is called at the end of the constructor
+	 * method.
+	 */
 	protected abstract void initialize();
-	
-	/** Disposes of the Screen's frame.
+
+	/**
+	 * Disposes of the Screen's frame.
 	 * 
 	 */
 	public void quit() {
 		frame.dispose();
 	}
-	
-	/** Makes the screen visible to the user.
+
+	/**
+	 * Makes the screen visible to the user.
 	 * 
 	 */
 	public void show() {
 		frame.setVisible(true);
 	}
-	
-	/** Hides the screen from the user
+
+	/**
+	 * Hides the screen from the user
 	 * 
 	 */
 	public void hide() {
 		frame.setVisible(false);
 	}
-	
-	/** Displays a Dialog to allow the user to confirm whether they would like to quit
+
+	/**
+	 * Displays a Dialog to allow the user to confirm whether they would like to
+	 * quit
 	 * 
 	 * @return Boolean true if would like to quit, else false
 	 */
 	protected void confirmQuit() {
-		int selection = JOptionPane.showConfirmDialog(frame, "Are you sure you want to quit?",
-                "Quit?", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+		int selection = JOptionPane.showConfirmDialog(frame, "Are you sure you want to quit?", "Quit?",
+				JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
 
-         if (selection == JOptionPane.YES_OPTION) {
-        	 quit();
-         }
+		if (selection == JOptionPane.YES_OPTION) {
+			quit();
+		}
 	}
 
-	/** Handles showing of a new screen, and hides this screen
-     * 
-     * @param newScreen Screen object to be switched to
-     */
-    public void swtichToNewScreen(Screen newScreen){
-        newScreen.show();
-        // this.hide();
-    }
+	/**
+	 * Handles showing of a new screen, and hides this screen
+	 * 
+	 * @param newScreen Screen object to be switched to
+	 */
+	public void swtichToNewScreen(Screen newScreen) {
+		newScreen.show();
+		// this.hide();
+	}
 
-	/** Handles switching back to a parent screen 
+	/**
+	 * Handles switching back to a parent screen
 	 * 
 	 */
-	public void switchToParent(){
-		if (parent == null){
+	public void switchToParent() {
+		if (parent == null) {
 			throw new IllegalStateException("This screen doesn't have a parent screen to switch to!");
-		}
-		else{
+		} else {
 			parent.show();
 			this.hide();
 		}
 	}
 
+	/**
+	 * Disables or enables a JButton according to parameter setting
+	 * 
+	 * @param btn     JButton to be enabled or not
+	 * @param setting Boolean value for if a JButton is to be enabled or not
+	 */
+	public void toggleButton(JButton btn, boolean setting) {
+		btn.setEnabled(setting);
+	}
 
 }
