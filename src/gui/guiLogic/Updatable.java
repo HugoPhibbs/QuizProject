eb17c580c06a2d5b715adc21b017cded3096dad8
hugophibbs <81545721+hugophibbs@users.noreply.github.com>
2021-07-31 15:15:12 +1200

    Renamed updatable

--- src/gui/guiLogic/Updatable.java
+++ src/gui/guiLogic/Updatable.java
@@ -4,16 +4,16 @@ package gui.guiLogic;
  * To be implemented by ScreenLogic classes that can change their state
  * according the change of state of the application.
  * <p>
- * Updateable classes are meant to be notified for changes by Updater Classes
+ * Updatable classes are meant to be notified for changes by Updater Classes
  * 
  * @author Hugo Phibbs
  * @version 13/7/21
  * @since 12/7/21
  */
-public interface Updateable {
+public interface Updatable {
 
     /**
-     * Handles recieving an update from an Updater class
+     * Handles receiving an update from an Updater class
      */
     public abstract void receiveUpdate();
 }
